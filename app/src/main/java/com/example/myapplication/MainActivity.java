25a0ba450ba278ebd1aa3da9029821c869d54c3a
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-24 03:26:51 +0700

    CheckBox dan RadioButton, serta ViewBinding(pengganti findViewById)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,41 @@
 package com.example.myapplication;
 
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-// AppCompatActivity : class di android framework  yang menjelaskan activity apa
-// dan mewarisi dari class itu untuk mengambil behavior/perilaku
+import com.example.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    // contoh penggunaan viewBinding / pengganti findViewById()
+    ActivityMainBinding binding;
 
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView sebelumnya akan digantikan oleh ViewBinding
+        // lalu di inisialisasi di sini
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        Button btnAddImage = findViewById(R.id.btnAddImage);
+        binding.btnOrder.setOnClickListener(v -> {
+            int checkMeatRadioButtonId = binding.rgMeat.getCheckedRadioButtonId();
+            RadioButton meat = findViewById(checkMeatRadioButtonId);
+            boolean cheese = binding.cbCheese.isChecked();
+            boolean onion = binding.cbOnion.isChecked();
+            boolean salad = binding.cbSalad.isChecked();
+            String orderSting = "Your order burger with : \n " +
+                    meat.getText() +
+                    (cheese ? "\nCheese" : "") +
+                    (onion ? "\nOnion" : "") +
+                    (salad ? "\nSalad" : "");
+            binding.tvOrder.setText(orderSting);
 
-        btnAddImage.setOnClickListener(v -> {
-            ImageView ivImage = findViewById(R.id.ivImage);
-
-            ivImage.setImageResource(R.drawable.fotos);
         });
     }
 }
