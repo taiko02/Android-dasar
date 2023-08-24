cc0059e060311dcb0def0178603d97505eac168c
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-25 04:50:09 +0700

    Intents dan start activities : dan cara penggunaan viewbinding di activity baru

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.myapplication;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,27 +17,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView sebelumnya akan digantikan oleh ViewBinding
-        // lalu di inisialisasi di sini
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-        binding.showToast.setOnClickListener(v -> {
-            // NORMAL TOAST
-            //Toast.makeText(getApplicationContext(), "Hi iam toast", Toast.LENGTH_LONG).show();
-
-            // CUSTOM TOAST
-            // Membuat instance LayoutInflater
-            LayoutInflater li = getLayoutInflater();
-            // Mendapatkan objek View seperti yang ditentukan dalam file custom_toast.xml
-            View layout = li.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.clToast));
-
-            // membuat toast object
-            Toast toast = new Toast(getApplicationContext());
-            toast.setDuration(Toast.LENGTH_LONG);
-            toast.setView(layout);
-            toast.show();
+        binding.btOpenActivity.setOnClickListener(v -> {
+            // agar bisa pindah activity kita perlu mendaftarkan ke manifest
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
         });
+
     }
 }
