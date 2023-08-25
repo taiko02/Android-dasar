5a6ffee0852722786bf3d9a7d87cca6de3ac3e7f
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-26 00:23:49 +0700

    Passing data / melempar data antar activity dengan data class seriazable

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,31 +1,40 @@
-package com.example.myapplication;
+package com.example.myapplication
 
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.myapplication.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
-
+class MainActivity : AppCompatActivity() {
     // contoh penggunaan viewBinding / pengganti findViewById()
-    ActivityMainBinding binding;
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        binding.btnApply.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val age = binding.etAge.text.toString().toInt()
+            val country = binding.etCountry.text.toString()
+            val person = Person(name,country,age)
 
-        binding.btOpenActivity.setOnClickListener(v -> {
-            // agar bisa pindah activity kita perlu mendaftarkan ke manifest
-            Intent intent = new Intent(this, SecondActivity.class);
-            startActivity(intent);
-        });
+            Intent(this,SecondAcitivity::class.java).also {
+                it.putExtra("EXTRA_PERSON",person) //person merupakan tipe object seriazable yang kita buat di data class
+
+                // cara dibawah ini adalah cara tanpa data class
+//                it.putExtra("EXTRA_NAME",name)
+//                it.putExtra("EXTRA_AGE", age)
+//                it.putExtra("EXTRA_COUNTRY", country)
+                startActivity(it)
+            }
+        }
+
 
     }
 }
